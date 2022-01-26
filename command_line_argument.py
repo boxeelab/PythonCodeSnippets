eff8c2e4a68b1857acb13fcea7fa35a120cb0787
Boxeelab <pingdjm@gmail.com>
2022-01-25 19:20:49 -0500

    Update command_line_argument.py

--- command_line_argument.py
+++ command_line_argument.py
@@ -1,1 +1,15 @@
 
+
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--dir', type = str, default = 'pet_images/', help = 'Command line text parameter') 
+parser.add_argument('--count', type = int, default = 2, help = 'Command line int paramter') 
+
+
+argument =  parser.parse_args()
+print(argument)
+
+#try python command_line_argument.py --count 10
+#try python command_line_argument.py --dir "test/"
+#try python command_line_argument.py -h for help
